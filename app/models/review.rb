494abc5dc950004bb48ef668c8beb5a11d2b3f6e
Rossella Pellizzi <prellizzi@gmail.com>
2023-08-16 19:04:54 +0200

    unfinished

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, :rating, :restaurant, presence: true
+  validates :rating, inclusion: { in: (0..5).to_a }
+  validates :rating, numericality: { only_integer: true }
+end
